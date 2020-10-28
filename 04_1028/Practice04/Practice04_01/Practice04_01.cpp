061c04e630d7c0b2b63de214369d1aa0b52d1775
kitagawa-souma <tyskfsouma@gmail.com>
2020-10-28 14:55:02 +0900

    第4回実習その1
    staticメンバ関数、メンバ変数について
     書き方・使い方
     注意点
     使用例

--- 04_1028/Practice04/Practice04_01/Practice04_01.cpp
+++ 04_1028/Practice04/Practice04_01/Practice04_01.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "Test.h"
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	Test testA;
+	Test testB;
+
+	testA.SetValue(10);
+	testA.SetStaticValue(9872);
+	testB.SetValue(345);
+	//staticメンバ関数は以下のように書くこともできる
+	Test::SetStaticValue(1000);
+
+	testA.PrintValue();
+	testB.PrintValue();
+
+	//ststicメンバ関数・変数の使い方1例（実体の数を数える）
+	Test::PrintCount();
+	//スコープ{}単体で運用できる
+	{
+		Test testC;
+		Test::PrintCount();
+	}
+	Test::PrintCount();
+
+	return 0;
 }
---
+++ 04_1028/Practice04/Practice04_01/Test.cpp
@@ -0,0 +1,53 @@
+#include "Test.h"
+#include <stdio.h>
+
+//staticメンバ変数(定義部)
+//型名 クラス名::変数名(= 初期値)
+//注:staticメンバ変数は、宣言と別に実体を作っておく必要がある
+//	実態を作るときは、必ずcpp側に書くこと
+int Test::staticvalue = 0;
+int Test::instsnceCount = 0;
+
+Test::Test()
+{
+	value = 0;
+
+	instsnceCount++;
+}
+
+Test::~Test()
+{
+	instsnceCount--;
+}
+
+void Test::SetValue(int value_)
+{
+	value = value_;
+}
+
+int Test::GetValue()
+{
+	return value;
+}
+
+void Test::PrintValue()
+{
+	printf("value = %d\n", value);
+	printf("staticvalue = %d\n", staticvalue);
+}
+
+//staticメンバ関数の書き方(定義部)
+//戻り値 関数名(引数)
+void Test::SetStaticValue(int value_)
+{
+	staticvalue = value_;
+
+	//注:staticメンバ関数の中では、
+	//static以外のメンバ変数を操作することはできない
+	//value = value_; //error
+}
+
+void Test::PrintCount()
+{
+	printf("Instance Count = %d\n", instsnceCount);
+}
