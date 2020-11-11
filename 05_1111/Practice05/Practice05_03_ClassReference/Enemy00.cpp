f11ee1e1f6a186cb80f8dd91beec4c5ed1f52d2c
kitagawa-souma <tyskfsouma@gmail.com>
2020-11-11 14:56:31 +0900

    第５回実習その３
    クラスと組み合わせた参照の使い方実践

---
+++ 05_1111/Practice05/Practice05_03_ClassReference/Enemy00.cpp
@@ -0,0 +1,11 @@
+#include "Enemy00.h"
+
+Enemy00::Enemy00()
+{
+	m_Hp = 100;
+}
+
+Enemy00::~Enemy00()
+{
+
+}
---
+++ 05_1111/Practice05/Practice05_03_ClassReference/Enemy00.h
@@ -0,0 +1,13 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include "CharacterBase.h"
+
+class Enemy00 : public CharacterBase
+{
+public:
+	Enemy00();
+	virtual ~Enemy00();
+};
+
+#endif // !ENEMY_H
