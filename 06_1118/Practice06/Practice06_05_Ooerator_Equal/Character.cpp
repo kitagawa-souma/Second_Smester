94dce3908130044af70d6c3e5efcbbc85089a485
kitagawa-souma <tyskfsouma@gmail.com>
2020-11-18 18:02:18 +0900

    第６回実習その５
    代入演算子(operator = )のオーバーロード実践
    コピーコンストラクタの呼び分けに注意する

---
+++ 06_1118/Practice06/Practice06_05_Ooerator_Equal/Character.cpp
@@ -0,0 +1,34 @@
+#include "Character.h"
+
+// デフォルトの引数付きコンストラクタ
+Character::Character()
+	:m_Job(Freeter)
+{
+	printf("通常のコンストラクタ\n");
+}
+
+Character::Character(const Character& ch)
+{
+	m_Job = ch.GetJob();
+	printf("コピーコンストラクタ\n");
+}
+
+
+Character::~Character()
+{
+	printf("デストラクタ\n");
+}
+
+//オペレーターの定義部分
+Character& Character::operator = (Character& ch)
+{
+	printf("代入演算子のオーバーロード\n");
+	m_Job = ch.GetJob();
+
+	/*this(thisポインタ)
+		オペレーターの左辺値のデータはこの関数を呼び出しているクラスのインスタンスである
+		実は、クラスの関数には、thisという、関数を呼び出したインスタンス自身を指す
+		専用のポインタ変数が隠れて存在している。
+	*/
+	return *this;
+}
---
+++ 06_1118/Practice06/Practice06_05_Ooerator_Equal/Character.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+/*代入演算子のオペレーター
+書き方
+	戻り値の型 operator =(引数);
+ 
+クラスのオペレーターとして用意する場合
+	Character& operator = (Character &ch);
+
+	戻り値の型
+		Character& クラスの参照型
+
+	引数
+		Character& ch 右辺のデータ型も参照型
+
+	呼び出し方の例
+		Character ch1;
+		Character ch2;
+*/
+
+class Character
+{
+public:
+
+	enum JOB
+	{
+		Freeter,
+		Warrior,
+		Wizard,
+		Priest,
+	};
+
+public:
+	// デフォルトの引数付きコンストラクタ
+	Character();
+	Character(const Character& ch);
+
+	virtual ~Character();
+
+	//代入演算子(=)のoperator
+	Character& operator = (Character& ch);
+
+	JOB GetJob() const { m_Job; }
+
+private:
+	JOB m_Job;
+
+protected:
+	int m_Hp;
+	int m_Mp;
+};
+#endif	// #ifndef CHARACTER_BASE_H
